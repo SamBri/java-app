c0dc39c9901f31cefb3180dfffc0495e7416e496
Samuel Brifo <lunarhive@gmail.com>
2025-07-06 13:47:54 +0000

    server built on java api not spring web mvc

--- src/com/app/controllers/ReliableController.java
+++ src/com/app/controllers/ReliableController.java
@@ -1,10 +1,6 @@
 package com.app.controllers;
 
 import org.jooq.Result;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -14,14 +10,10 @@ import org.jooq.Record;
 
 import com.app.dao.ApplicationCursorDao;
 
-@RestController
-@RequestMapping("/reliable")
-@CrossOrigin(origins = "*") // enable cross origin on the controller level for all origins
-//@Slf4j
+
 public class ReliableController {
 
 	// fetch all app cursors.
-	@GetMapping("/application-cursors")
 	public List<?> fetchCursors() {
 
 		Result<Record> cursors = new ApplicationCursorDao().fetchCursors();
