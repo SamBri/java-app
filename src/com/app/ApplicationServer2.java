6929f41ce91cfb5b96eaf0fbed87b97511b7c390
samue <lunarhive@gmail.com>
2025-08-07 07:12:46 +0000

    make the application resilent

--- src/com/app/ApplicationServer2.java
+++ src/com/app/ApplicationServer2.java
@@ -4,6 +4,7 @@ package com.app;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+import com.app.controllers.ResilentServiceControllerImpl;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
@@ -21,7 +22,7 @@ public class ApplicationServer2 {
 	
 		HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
        
-		server.createContext("/", new APIControllerHandlerImpl2() );
+		server.createContext("/", new ResilentServiceControllerImpl() );
         server.setExecutor(null); // creates a default executor
         server.start();
         System.out.println("Server started on port 8001");
