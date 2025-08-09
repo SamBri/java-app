6089f12fc59963f19848a711576b227e2af48046
samue <lunarhive@gmail.com>
2025-08-09 18:32:29 +0000

    uses bucket operators

--- src/com/app/bucket/Bucket.java
+++ src/com/app/bucket/Bucket.java
@@ -1,39 +1,45 @@
 package com.app.bucket;
 
 //Amazon S3 - Bucket. John Doe
-public class Bucket {
+public class Bucket implements BucketOperator {
+
+	@Override
+	public Bucket init() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bucket load() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bucket create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bucket store(Object... objects) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bucket retrieve(int... ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bucket search(int... ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
-   // init
-   public Bucket init() {
-	   return this;
-   }
-   
-   // load
-   public Bucket load() {
-	   return this;
-   }
-   
-   // create
-   public Bucket create() {
-	   return this;
-   }
-   
-   // store
-   public Bucket store(Object... objects) {
-	   return this;
-   }
-   
-   
-   // retrieve
-   public Bucket retrieve(int... ids) {
-	   return this;
-   }
-   
-   
-   // search
-   public Bucket search(int... ids) {
-	   return this;
-   }
+  
 	
 	
 
