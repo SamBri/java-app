913ff51387a9a079b8294e091936df2e2ecab17a
samue <lunarhive@gmail.com>
2025-08-09 18:28:34 +0000

    create bucket operators

--- src/com/app/bucket/Bucket.java
+++ src/com/app/bucket/Bucket.java
@@ -3,6 +3,37 @@ package com.app.bucket;
 //Amazon S3 - Bucket. John Doe
 public class Bucket {
 	
+   // init
+   public Bucket init() {
+	   return this;
+   }
+   
+   // load
+   public Bucket load() {
+	   return this;
+   }
+   
+   // create
+   public Bucket create() {
+	   return this;
+   }
+   
+   // store
+   public Bucket store(Object... objects) {
+	   return this;
+   }
+   
+   
+   // retrieve
+   public Bucket retrieve(int... ids) {
+	   return this;
+   }
+   
+   
+   // search
+   public Bucket search(int... ids) {
+	   return this;
+   }
 	
 	
 
