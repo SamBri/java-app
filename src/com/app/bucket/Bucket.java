854c0184f948978a67c09fc299bebb8afae303a8
samue <lunarhive@gmail.com>
2025-08-09 18:54:55 +0000

    define bucket sample contract

--- src/com/app/bucket/Bucket.java
+++ src/com/app/bucket/Bucket.java
@@ -1,8 +1,11 @@
 package com.app.bucket;
 
+import java.io.File;
+
 //Amazon S3 - Bucket. John Doe
 public class Bucket implements BucketOperator {
 
+	
 	@Override
 	public Bucket init() {
 		return this;
@@ -32,17 +35,31 @@ public class Bucket implements BucketOperator {
 	public Bucket search(int... ids) {
 		return this;
 	}
-	
-	
-	
-	public static void main(String[] args) {
-		
-		// sample contract.
-		Bucket memories = new Bucket();
-		
+
+	@Override
+	public Bucket list() {
+		return this;
 	}
-  
-	
-	
+
+	public static void main(String[] args) {
+
+		// sample contract - 1;
+		Bucket memories = new Bucket();
+		memories = memories.init().create();
+		memories = memories.load();
+
+		File[] photos = new File[] { new File("mall.jpg"), new File("restaurant.jpg"), new File("instabul.jpg") };
+		File[] music = new File[] { new File("hustling.mp3"), new File("win.mp3"), new File("mozart.mp3") };
+		
+		memories = memories.store(photos, music);
+		memories = memories.search(null);
+		memories.list();
+		
+		// sample contract - 2;
+		Bucket artifacts = new Bucket();
+		artifacts.init().create().load().store(music, photos).list();
+		
+
+	}
 
 }
