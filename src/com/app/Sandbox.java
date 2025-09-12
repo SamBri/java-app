3b571cb18607904510d031579a5b4fc4e5d862ad
VICTUS\samue <lunarhive@gmail.com>
2025-09-12 22:19:45 +0000

    demo method overloading

---
+++ src/com/app/Sandbox.java
@@ -0,0 +1,41 @@
+package com.app;
+
+public class Sandbox {
+	
+	
+	// method overloading, fancy.
+	static void set(char signal) {
+		
+	}
+	
+	static void set(int number) {
+		
+	}
+	
+	static void set(String key) {
+		
+	}
+	
+	static void set(String key, int number) {
+		
+	}
+	
+    static void set(int number, String domain) {
+		
+	}
+
+
+	
+	public static void main(String[] args) {
+		
+
+		set('c');
+		set(911);
+		set("foo");
+		set("foo",100);
+		set(100,"100.com");
+		
+		
+	}
+	
+}
